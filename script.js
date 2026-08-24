06092d02aa353bd3fb1fd873aa68125af38b87e8
DominikCarreon <dvcarreon@student.apc.edu.ph>
2026-08-24 18:51:49 +0000

    aaa

--- script.js
+++ script.js
@@ -122,11 +122,8 @@ for (let i = 0; i < totalPetals; i++) {
   petal.style.fontSize = `${0.9 + Math.random() * 0.9}rem`;
   petalsContainer.appendChild(petal);
 }
-
-document.getElementById('start-btn').addEventListener('click', function() {
+document.addEventListener('click', function() {
   const music = document.getElementById('bg-music');
-  const startScreen = document.getElementById('start-screen');
-
   music.play().catch(error => console.log(error));
-  startScreen.style.display = 'none';
-});
+}, { once: true });
+
